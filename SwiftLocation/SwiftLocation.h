6aa7798e25b79e880b3e31df89fee077fb6fe4c5
zhihui.li <lizhihui02152@gmail.com>
2021-04-24 04:44:40 +0800

    carthage

---
+++ SwiftLocation/SwiftLocation.h
@@ -0,0 +1,18 @@
+//
+//  SwiftLocation.h
+//  SwiftLocation
+//
+//  Created by lizhihui on 2021/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftLocation.
+FOUNDATION_EXPORT double SwiftLocationVersionNumber;
+
+//! Project version string for SwiftLocation.
+FOUNDATION_EXPORT const unsigned char SwiftLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLocation/PublicHeader.h>
+
+
---
+++ SwiftLocationBeaconBroadcaster/SwiftLocationBeaconBroadcaster.h
@@ -0,0 +1,18 @@
+//
+//  SwiftLocationBeaconBroadcaster.h
+//  SwiftLocationBeaconBroadcaster
+//
+//  Created by lizhihui on 2021/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftLocationBeaconBroadcaster.
+FOUNDATION_EXPORT double SwiftLocationBeaconBroadcasterVersionNumber;
+
+//! Project version string for SwiftLocationBeaconBroadcaster.
+FOUNDATION_EXPORT const unsigned char SwiftLocationBeaconBroadcasterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLocationBeaconBroadcaster/PublicHeader.h>
+
+
